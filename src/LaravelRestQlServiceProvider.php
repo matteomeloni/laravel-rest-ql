15c8e4b6cae3de123865cce0cc7e6f07409ea715
Matteo Meloni <matteomelonig@gmail.com>
2021-11-24 11:24:05 +0100

    clean service provider

--- src/LaravelRestQlServiceProvider.php
+++ src/LaravelRestQlServiceProvider.php
@@ -13,15 +13,7 @@ class LaravelRestQlServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'matteomeloni');
-        // $this->loadViewsFrom(__DIR__.'/../resources/views', 'matteomeloni');
-        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-        // $this->loadRoutesFrom(__DIR__.'/routes.php');
 
-        // Publishing is only necessary when using the CLI.
-        if ($this->app->runningInConsole()) {
-            $this->bootForConsole();
-        }
     }
 
     /**
@@ -31,12 +23,7 @@ class LaravelRestQlServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        $this->mergeConfigFrom(__DIR__.'/../config/laravel-rest-ql.php', 'laravel-rest-ql');
 
-        // Register the service the package provides.
-        $this->app->singleton('laravel-rest-ql', function ($app) {
-            return new LaravelRestQl;
-        });
     }
 
     /**
@@ -46,7 +33,7 @@ class LaravelRestQlServiceProvider extends ServiceProvider
      */
     public function provides()
     {
-        return ['laravel-rest-ql'];
+
     }
 
     /**
@@ -56,27 +43,6 @@ class LaravelRestQlServiceProvider extends ServiceProvider
      */
     protected function bootForConsole(): void
     {
-        // Publishing the configuration file.
-        $this->publishes([
-            __DIR__.'/../config/laravel-rest-ql.php' => config_path('laravel-rest-ql.php'),
-        ], 'laravel-rest-ql.config');
 
-        // Publishing the views.
-        /*$this->publishes([
-            __DIR__.'/../resources/views' => base_path('resources/views/vendor/matteomeloni'),
-        ], 'laravel-rest-ql.views');*/
-
-        // Publishing assets.
-        /*$this->publishes([
-            __DIR__.'/../resources/assets' => public_path('vendor/matteomeloni'),
-        ], 'laravel-rest-ql.views');*/
-
-        // Publishing the translation files.
-        /*$this->publishes([
-            __DIR__.'/../resources/lang' => resource_path('lang/vendor/matteomeloni'),
-        ], 'laravel-rest-ql.views');*/
-
-        // Registering package commands.
-        // $this->commands([]);
     }
 }
